0b976b8bfa5386f5685b289dd6e5804cf34d7ea1
Presiyana Borisova <presiana03@gmail.com>
2025-02-18 18:05:53 +0200

    http client for artist added

--- src/main/java/com/trails_art/trails/TrailsApplication.java
+++ src/main/java/com/trails_art/trails/TrailsApplication.java
@@ -1,9 +1,14 @@
 package com.trails_art.trails;
 
+import com.trails_art.trails.clients.ArtistHttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @SpringBootApplication
 public class TrailsApplication {
@@ -15,4 +20,10 @@ public class TrailsApplication {
 		log.info("Application started successfully");
 	}
 
+	@Bean
+	ArtistHttpClient artistHttpClient() {
+		RestClient restClient = RestClient.create("http://localhost:8080/");
+		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+		return factory.createClient(ArtistHttpClient.class);
+	}
 }
