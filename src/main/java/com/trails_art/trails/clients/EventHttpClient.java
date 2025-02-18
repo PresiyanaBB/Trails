3df38be59dc1d82a32ff1cdf7da253324d822066
Presiyana Borisova <presiana03@gmail.com>
2025-02-18 19:16:58 +0200

    http clients added

--- src/main/java/com/trails_art/trails/clients/EventHttpClient.java
+++ src/main/java/com/trails_art/trails/clients/EventHttpClient.java
@@ -1,4 +1,43 @@
 package com.trails_art.trails.clients;
 
+import com.trails_art.trails.models.Event;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping()
 public interface EventHttpClient {
+    @GetMapping()
+    List<Event> findAll();
+
+    @GetMapping("/{id}")
+    Event findById(@PathVariable UUID id);
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping()
+    void create(@Valid @RequestBody Event event);
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    void update(@Valid @RequestBody Event event,@PathVariable UUID id);
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable UUID id);
+
+    @GetMapping("/count")
+    int count();
+
+    @GetMapping("/{name}")
+    List<Event> findByName(@PathVariable String name);
 }
